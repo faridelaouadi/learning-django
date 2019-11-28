a6fe24368ce8288830f6fd9bca40ce03c04b8e9e
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2019-11-28 12:33:11 +0000

    added sidebar and finished UI

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,14 +1,51 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Tutorial, TutorialCategory, TutorialSeries
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
 from .forms import NewUserForm
 
+
 # Create your views here.
 
+def single_slug(request, single_slug):
+    # first check to see if the url is in categories.
+
+    categories = [c.category_slug for c in TutorialCategory.objects.all()]
+    if single_slug in categories:
+        matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug=single_slug)
+        series_urls = {}
+
+        for m in matching_series.all():
+            part_one = Tutorial.objects.filter(tutorial_series__tutorial_series=m.tutorial_series).earliest("tutorial_published")
+            series_urls[m] = part_one.tutorial_slug
+
+        return render(request=request,
+                      template_name='main/category.html',
+                      context={"tutorial_series": matching_series, "part_ones": series_urls})
+
+
+    tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
+
+    if single_slug in tutorials:
+        this_tutorial = Tutorial.objects.get(tutorial_slug=single_slug)
+        tutorials_from_series = Tutorial.objects.filter(tutorial_series__tutorial_series=this_tutorial.tutorial_series).order_by('tutorial_published')
+        this_tutorial_idx = list(tutorials_from_series).index(this_tutorial)
+
+        return render(request=request,
+                      template_name='main/tutorial.html',
+                      context={"tutorial":this_tutorial,
+                               "sidebar": tutorials_from_series,
+                               "this_tut_idx": this_tutorial_idx})
+
+    return HttpResponse("404 error")
+
+
+
+
 def homepage(request):
-    return render(request, "main/home.html", {"tutorials":Tutorial.objects.all})
+    return render(request, "main/categories.html", {"categories":TutorialCategory.objects.all})
 
 def register(request):
     if request.method == "POST":
